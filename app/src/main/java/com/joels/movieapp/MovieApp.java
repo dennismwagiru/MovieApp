af4cb532b7c8931dbf76e6f82add6661a100586c
Dennis Joel Mwagiru <dennismwagiru@gmail.com>
2018-07-05 08:58:44 +0300

    Setting up dependencies

---
+++ app/src/main/java/com/joels/movieapp/MovieApp.java
@@ -0,0 +1,11 @@
+package com.joels.movieapp;
+
+import android.support.multidex.MultiDexApplication;
+
+public class MovieApp extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/joels/movieapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/joels/movieapp/ui/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.joels.movieapp;
+package com.joels.movieapp.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.joels.movieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
